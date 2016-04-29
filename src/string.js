be95326ba080db72bfdc1fcc701d17d917699fdc
diomabros <francesco@diomabros.it>
2016-04-29 16:44:56 +0200

    Added new functions
    ltrim
    rtrim
    trimOff
    capitalizeFirstLetter

--- src/string.js
+++ src/string.js
@@ -34,6 +34,88 @@ if(!inString('endsWith')) {
 	
 }
 
+if(!inString('capitalizeFirstLetter')) {
+	
+	//Add JSDoc comment
+	String.prototype.capitalizeFirstLetter = function () {
+	    return this.charAt(0).toUpperCase() + this.slice(1);
+	}
+	
+}
+
+if(!inString('rtrim')) {
+	
+	//Add JSDoc comment
+	String.prototype.rtrim = function(toCut){
+	    if (!toCut)
+	        toCut = " ";
+	    
+	    if (typeof toCut !== 'string')
+	        throw "parameter must be string, " + (typeof toCut) + " passed";
+	    
+	    var toReturn = this;
+	    
+	    var last = toReturn.lastIndexOf(toCut);
+	    if (last > 0)
+	    {
+	        while (last >= 0)
+	        {
+	            toReturn = toReturn.slice(0, last);
+	            last = toReturn.lastIndexOf(toCut);
+	        }
+	    }
+	    else
+	        return this;
+	    
+	    return toReturn;    
+	}
+	
+}
+
+if(!inString('ltrim')) {
+	
+	//Add JSDoc comment
+	String.prototype.ltrim = function(toCut){
+	    if (!toCut)
+	        toCut = " ";
+	    
+	    if (typeof toCut !== 'string')
+	        throw "parameter must be string, " + (typeof toCut) + " passed";
+	    
+	    var toReturn = this;
+	    
+	    var first = toReturn.indexOf(toCut);
+	    if (first !== 0)
+	        return this;
+	    else
+	    {
+	        while (first === 0)
+	        {
+	            toReturn = toReturn.slice(toCut.length, toReturn.length);
+	            first = toReturn.indexOf(toCut);
+	        }
+	    }
+	    
+	    return toReturn;    
+	}
+	
+}
+
+if(!inString('trimOff')) {
+	
+	//Add JSDoc comment
+	String.prototype.trimOff = function(toCut){
+	    if (!toCut)
+	        toCut = " ";
+	    
+	    if (this.rtrim && this.ltrim)
+	        return this.rtrim(toCut).ltrim(toCut);
+	    else
+	        throw "rtrim and ltrim prototypes required";
+	}
+	
+}
+
 if(!inString('random')) {
 	
 	//Add JSDoc comment
